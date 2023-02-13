36a772e6c8c5f344cd1bd7570b1cc9d697953d5d
dominikusvieri <dominikusvieri9@gmail.com>
2023-02-13 19:56:32 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap'
+import Login from './components/Login';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Register from './components/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home title='Home Page' />} />
+          <Route path='/dashboard' element={<Dashboard title='Dashboard' />} />
+          <Route path='/login' element={<Login title='Login Page' />} />
+          <Route path="/register" element={<Register title="REGISTER PAGE" description="MINI ABSENSI APPS" />} />
+          <Route path='*' element={<h1>Page not found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
